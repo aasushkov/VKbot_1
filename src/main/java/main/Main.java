3edb50ff4b7852f239eef38c824ac7c0dab03675
aasushkov <v96584@gmail.cim>
2017-01-26 00:20:39 +0400

    //version 1.0 todo testing count comments

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,28 +1,42 @@
 package main;
 
-import org.apache.http.HeaderElement;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import vkbot.VKautorize;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        VKautorize vKautorize;
 
         Scanner in = new Scanner(System.in);
         System.out.print("Enter VK Login: ");
         String LOGIN = in.nextLine();
         System.out.print("Enter VK Password: ");
         String PASS = in.nextLine();
-        System.out.print("Enter VK Group: ");
-        String GROUP = in.nextLine();
-        System.out.print("Enter Text Comment: ");
-        String COMMENT = in.nextLine();
+        String GROUP;
+        String COMMENT;
 
-        VKautorize vKautorize = new VKautorize(LOGIN,PASS,GROUP,COMMENT);
 
-        vKautorize.sendComment();
+        String s;
+        BufferedReader brComment = new BufferedReader
+                (new InputStreamReader(new FileInputStream("comment.txt"), "CP1251"));
+        StringBuffer commentBuffer = new StringBuffer();
+        while ((s = brComment.readLine()) != null) {
+            commentBuffer.append(s);
+        }
+        COMMENT = commentBuffer.toString();
 
-           }
+
+        BufferedReader brGroupList = new BufferedReader
+                (new InputStreamReader(new FileInputStream("groupList.txt"), "CP1251"));
+        while ((GROUP = brGroupList.readLine()) != null) {
+            vKautorize = new VKautorize(LOGIN, PASS, GROUP, COMMENT);
+            vKautorize.sendComment();
+        }
+
+        brGroupList.close();
+    }
+
 }
